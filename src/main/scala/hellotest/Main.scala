58186fdd1dcbd31748c8d43e8977b97084160891
Elizabeth Bastow <libbycompstuff@gmail.com>
2024-09-26 15:54:57 +0000

    Changed code to accept command line args, but now when you try to run, it says no @main methods declared

--- src/main/scala/hellotest/Main.scala
+++ src/main/scala/hellotest/Main.scala
@@ -19,49 +19,41 @@ object Main:
   def initLogging(): Unit = {
     // Log4s doesn't have setLoggerThreshold, etc. We configure log levels via application configuration.
     
-    // Loggers are configured directly using logger names. You typically do this in a configuration file.
-    // Here is an example of logging at different levels:
+    // Loggers are configured directly using logger names. 
     logger.trace("This is a trace message") // Will not be printed if level is set to higher
     logger.debug("This is a debug message") // Will not be printed if level is set to higher
     logger.info("This is an info message") // Will be printed
     logger.warn("This is a warning message") // Will be printed
     logger.error("This is an error message") // Will be printed
-
-
-    // Since there are no ways to add appenders. Log4s does not allow you to add appenders directly in this way.
-    // You would typically set appenders in a logging configuration file (e.g., log4s.xml or log4s.properties).
   }
 
   def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args.toIndexedSeq)
   @main
+    
   def run(
-    @arg(short = 'c', doc = "size of the sliding word cloud") cloudSize: Int = 10,
-    @arg(short = 'l', doc = "minimum word length to be considere") minLength: Int = 6,
-    @arg(short = 'w', doc = "size of the sliding FIFO queue") windowSize: Int = 1000,
+    @arg(short = 'c', doc = "size of the sliding word cloud") cloudSize: Int = CLOUD_SIZE,
+    @arg(short = 'l', doc = "minimum word length to be considered") minLength: Int = LENGTH_AT_LEAST,
+    @arg(short = 'w', doc = "size of the sliding FIFO queue") windowSize: Int = WINDOW_SIZE,
     @arg(short = 's', doc = "number of steps between word cloud updates") everyKSteps: Int = 10,
     @arg(short = 'f', doc = "minimum frequency for a word to be included in the cloud") minFrequency: Int = 3) =
 
-  logger.debug(f"howMany=$cloudSize minLength=$minLength lastNWords=$windowSize everyKSteps=$everyKSteps minFrequency=$minFrequency")
-
-    // Parse the command-line argument or use the default value
-    var cloud_size = CLOUD_SIZE
-    var length_at_least = LENGTH_AT_LEAST
-    var window_size = WINDOW_SIZE
+    logger.debug(f"howMany=$cloudSize minLength=$minLength lastNWords=$windowSize everyKSteps=$everyKSteps minFrequency=$minFrequency")
+    
+    
 
     // Set up input from stdin and process words
     val lines = scala.io.Source.fromInputStream(System.in)("UTF-8").getLines
 
     val words = lines.flatMap(l => l.split("(?U)[^\\p{Alpha}0-9']+")).map(_.toLowerCase)
 
-
-    val queue = new CircularFifoQueue[String](window_size)
+    val queue = new CircularFifoQueue[String](windowSize)
 
     // Process words and update word cloud
-    words.filter(_.length >= length_at_least).foreach { word =>
+    words.filter(_.length >= minLength).foreach { word =>
       queue.add(word) // Add word to the queue
 
       // start processing when the queue is full with window_size
-      if (queue.size == window_size) {
+      if (queue.size == windowSize) {
         val wordCount = mutable.Map[String, Int]()
         queue.forEach { w =>
           wordCount(w) = wordCount.getOrElse(w, 0) + 1
@@ -69,9 +61,13 @@ object Main:
 
         val sortedWords = wordCount.toSeq.sortBy { case (word, count) => (-count, word) }
 
-         val topWords = sortedWords.take(cloud_size)
+         val topWords = sortedWords.take(cloudSize)
 
         println(topWords.map { case (word, count) => s"$word: $count" }.mkString(" "))
+        if (System.out.checkError()) {
+          println("Error writing to stdout. Exiting.")
+          sys.exit(1)  // Exit with a failure code
+        }
       }
     }
 
